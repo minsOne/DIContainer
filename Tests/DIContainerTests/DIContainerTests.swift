327f88a4f8bc154381febaa554161f8e2d74e28d
damon.ahn <damon.ahn@kakaobank.com>
2023-01-11 14:38:40 +0900

    update test code

--- Tests/DIContainerTests/DIContainerTests.swift
+++ Tests/DIContainerTests/DIContainerTests.swift
@@ -15,15 +15,25 @@ final class DIContainerTests: XCTestCase {
         .build()
     }
 
-    func test_컨테이너_등록여부_확인() {
+    func test_컨테이너_등록여부_확인_1() {
         XCTAssertNotNil(MockServiceKey.module?.resolve() as? MockServiceKey.Value)
         XCTAssertNotNil(MockServiceKey.module?.resolve() as? MockService)
     }
+
+    func test_컨테이너_등록여부_확인_2() {
+        @Inject(MockServiceKey.self) var service; _ = service
+    }
     
-    func test_Inject동작확인() {
+    func test_Inject동작확인_1() {
         @Inject(MockServiceKey.self)
         var service: MockService
         
+        service.doSomething()
+        XCTAssertEqual(mock.count, 1)
+    }
+
+    func test_Inject동작확인_2() {
+        @Inject(MockServiceKey.self) var service;
         service.doSomething()
         XCTAssertEqual(mock.count, 1)
     }
